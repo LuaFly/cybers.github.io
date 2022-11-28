9d07b3f9639c651a62892977e95a8ba5d3262645
LuaFly <luasoestudos@yahoo.com>
2022-11-28 08:42:08 -0300

    fonte idioma e foto

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// var dataReload = document.querySelectorAll("a[data-reload]");
+// var hi = document.getElementById("menu");
+// var language = {
+//   eng: {
+//     welcome: "Welcome everyone!"
+//   },
+
+//   ita: {
+//     welcome: "Benvenuti a tutti!"
+//   }
+
+// };
+
+// var setLanguage = () => {
+
+//   if (window.location.hash === "#ita") {
+//     hi.textContent = language.ita.welcome;
+//   } else {
+//     hi.textContent = language.eng.welcome;
+//   }
+
+// }
+
+// for (i = 0; i < dataReload.length; i++) {
+//   dataReload[i].onclick = function(e) {
+//     window.location.hash = this.getAttribute("href");
+//     setLanguage();
+//   }
+// }
+
+// setLanguage();
+
